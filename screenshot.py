f9042a8b0033d136db1452d189b11e288ded61a2
XCI9 <chenyutse913@gmail.com>
2022-05-24 16:31:54 +0800

    mix two different ways to screenshot.

--- screenshot.py
+++ screenshot.py
@@ -1,15 +1,19 @@
 from mss import mss
 from PIL import Image
 import numpy as np
+import pyautogui
 
 sct = mss()
 def screenshot(left, top, width, height):
-    mon = {'left': left, 'top': top, 'width': width, 'height': height}
-    screenShot = sct.grab(mon)
-    img = np.array(Image.frombytes(
-        'RGB', 
-        (screenShot.width, screenShot.height), 
-        screenShot.rgb, 
-    ))
+    if width * height < 1286000:
+        mon = {'left': left, 'top': top, 'width': width, 'height': height}
+        screenShot = sct.grab(mon)
+        img = np.array(Image.frombytes(
+            'RGB', 
+            (screenShot.width, screenShot.height), 
+            screenShot.rgb, 
+        ))
+    else:
+        img = np.array(pyautogui.screenshot())[left:left+width, top:top+height]
 
     return img
