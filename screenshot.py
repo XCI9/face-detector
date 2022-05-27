4fe711ec0b91e4fabb58f947582b5bfb01814c34
XCI9 <chenyutse913@gmail.com>
2022-05-27 16:42:22 +0800

    Screenshot faster.

--- screenshot.py
+++ screenshot.py
@@ -1,19 +1,9 @@
 from mss import mss
-from PIL import Image
 import numpy as np
-import pyautogui
 
 sct = mss()
 def screenshot(left, top, width, height):
-    if width * height < 1286000:
-        mon = {'left': left, 'top': top, 'width': width, 'height': height}
-        screenShot = sct.grab(mon)
-        img = np.array(Image.frombytes(
-            'RGB', 
-            (screenShot.width, screenShot.height), 
-            screenShot.rgb, 
-        ))
-    else:
-        img = np.array(pyautogui.screenshot())[left:left+width, top:top+height]
+    mon = {'left': left, 'top': top, 'width': width, 'height': height}
+    img = np.asarray(sct.grab(mon))
 
     return img
